c4a746da3b0b6903c09e9165febdc7787360f591
Sendibayu2210 <nugraha102210@gmail.com>
2022-09-22 08:33:47 +0700

    delete slash

--- app/Views/template.php
+++ app/Views/template.php
@@ -6,9 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $title; ?></title>
 
-    <script src="/vendor/jquery/jquery-3.6.1.min.js"></script>
-    <link rel="stylesheet" href="/vendor/bootstrap5/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/vendor/fontawesome612/css/all.css">
+    <script src="vendor/jquery/jquery-3.6.1.min.js"></script>
+    <link rel="stylesheet" href="vendor/bootstrap5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="vendor/fontawesome612/css/all.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;1,300&display=swap" rel="stylesheet">
     <style>
         * {
@@ -21,8 +21,8 @@
 <body>
     <?= $this->renderSection('content'); ?>
 
-    <script src="/vendor/bootstrap5/js/bootstrap.bundle.min.js"></script>
-    <script src="/vendor/fontawesome612/js/all.min.js"></script>
+    <script src="vendor/bootstrap5/js/bootstrap.bundle.min.js"></script>
+    <script src="vendor/fontawesome612/js/all.min.js"></script>
 </body>
 
 </html>
